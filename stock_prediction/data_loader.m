12c26cfb18b535028011b1cf92d947209fd96771
Ning <allenwei0503@gmail.com>
2024-08-20 01:44:43 -0500

    Add files via upload
    update dataloader

--- stock_prediction/data_loader.m
+++ stock_prediction/data_loader.m
@@ -1,14 +1,25 @@
 function [training_x, training_y] = data_loader(file_root, cols_x, row_start_x, row_end_x, cols_y, row_start_y, row_end_y)
-    % 构造完整的读取区域，确保从 cell 数组中提取字符串
-    range_x = sprintf('%s%d:%s%d', cols_x{1}, row_start_x, cols_x{end}, row_end_x);
-    range_y = sprintf('%s%d:%s%d', cols_y{1}, row_start_y, cols_y{end}, row_end_y);
+    
+    training_x_parts = cell(1, numel(cols_x));
+    training_y_parts = cell(1, numel(cols_y));
+    
+    
+    for i = 1:numel(cols_x)
+        range_x = sprintf('%s%d:%s%d', cols_x{i}, row_start_x, cols_x{i}, row_end_x);
+        temp_x = readtable(file_root, 'Range', range_x);
+        temp_x.Properties.VariableNames = {cols_x{i}};  
+        training_x_parts{i} = temp_x;
+    end
+    
+    training_x = [training_x_parts{:}];
 
-    % 读取指定的列，处理单列和多列的情况
-    opts_x = detectImportOptions(file_root, 'Range', range_x);
-    opts_x.SelectedVariableNames = cols_x;  % 确保只选取指定列
-    training_x = readtable(file_root, opts_x);
-
-    opts_y = detectImportOptions(file_root, 'Range', range_y);
-    opts_y.SelectedVariableNames = cols_y;  % 确保只选取指定列
-    training_y = readtable(file_root, opts_y);
+   
+    for i = 1:numel(cols_y)
+        range_y = sprintf('%s%d:%s%d', cols_y{i}, row_start_y, cols_y{i}, row_end_y);
+        temp_y = readtable(file_root, 'Range', range_y);
+        temp_y.Properties.VariableNames = {cols_y{i}};  
+        training_y_parts{i} = temp_y;
+    end
+  
+    training_y = [training_y_parts{:}];
 end
